7804ab64d3336a5f74feda2631f40d838e15f246
Shittu24 <42889574+Shittu24@users.noreply.github.com>
2018-09-10 09:31:23 +0100

    Add files via upload

---
+++ Loops.py
@@ -0,0 +1,46 @@
+#x = 0
+#while(x <= 100):
+    #print(x)
+    #x+= 1
+
+
+#Increasing
+#for i in range(1,11):
+#    print(i)
+
+#Decreasing
+#for i in range(10,0,-1):
+  #  print(i)
+
+
+#x = 0
+#while(x <= 100):
+ #   x+= 1
+  #  if(x%3 == 0 and x%5 == 0):
+   #     print("FizzBuzz")
+    #elif(x%3 == 0):
+     #  print("Fizz")
+    #elif(x%5== 0):
+     #  print("Buzz")
+    #else:
+     #   print(x)
+
+
+
+for i in range(1,101):
+    
+    if(i%3 == 0 and i%5 == 0):
+        print("FizzBuzz")
+    elif(i%3 == 0):
+       print("Fizz")
+    elif(i%5== 0):
+       print("Buzz")
+    else:
+        print(i)
+    
+    
+    
+
+    
+
+ 
